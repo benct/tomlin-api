1f92f65018735f6d4a859d82ceda1b494fe1ad06
benct <ben@tomlin.no>
2019-07-22 01:27:30 +0200

    Add basic JDBC setup

--- src/main/kotlin/no/tomlin/api/ApiController.kt
+++ src/main/kotlin/no/tomlin/api/ApiController.kt
@@ -2,6 +2,8 @@ package no.tomlin.api
 
 import no.tomlin.api.common.Constants.ADMIN
 import no.tomlin.api.common.Constants.USER
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ApiController {
 
+    @Autowired
+    private lateinit var jdbcTemplate: JdbcTemplate
+
     @GetMapping("/ping")
     fun ping() = "pong"
+
+    @Secured(ADMIN)
+    @GetMapping("/db")
+    fun dbTest() = jdbcTemplate.queryForList("SELECT * FROM tomlin_flight")
 
     @Secured(USER, ADMIN)
     @GetMapping("/auth")
