3abd2db14d8998a3a6c8ae3ce3c2fbe68a030be3
benct <ben@tomlin.no>
2019-07-22 01:24:10 +0200

    Rename application and use yaml properties

--- src/main/kotlin/no/tomlin/api/ApiApplication.kt
+++ src/main/kotlin/no/tomlin/api/ApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TomlinApiApplication
+class ApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<TomlinApiApplication>(*args)
+    runApplication<ApiApplication>(*args)
 }
--- src/test/kotlin/no/tomlin/api/ApiApplicationTests.kt
+++ src/test/kotlin/no/tomlin/api/ApiApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class TomlinApiApplicationTests {
+class ApiApplicationTests {
 
     @Test
     fun contextLoads() {
