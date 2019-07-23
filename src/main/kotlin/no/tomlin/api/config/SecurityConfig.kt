8a22eb0804e045df44eda02f481285faaa195342
benct <ben@tomlin.no>
2019-07-23 19:54:37 +0200

    Improve security configuration

--- src/main/kotlin/no/tomlin/api/config/SecurityConfig.kt
+++ src/main/kotlin/no/tomlin/api/config/SecurityConfig.kt
@@ -1,27 +1,31 @@
 package no.tomlin.api.config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance
+import org.springframework.security.config.http.SessionCreationPolicy
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
-    @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.httpBasic()
+        http.csrf().disable()
+            .formLogin().disable()
+            .logout().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .httpBasic()
+//            .and()
+//            .requiresChannel().anyRequest().requiresSecure()
     }
 
-    @Autowired
-    @Throws(Exception::class)
-    fun configureGlobal(auth: AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(getInstance())
-                .withUser("ben@tomlin.no").password("xxxxxx").roles("ADMIN", "USER")
-    }
+//    override fun configure(auth: AuthenticationManagerBuilder) {
+//        auth.inMemoryAuthentication()
+//            .passwordEncoder(BCryptPasswordEncoder(12))
+//            .withUser(username)
+//            .password(password)
+//            .roles("ADMIN", "USER")
+//    }
 }
