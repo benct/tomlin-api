8337cf907b2a36b764cb6378879240852acacba7
benct <ben@tomlin.no>
2019-12-24 01:52:18 +0100

    File uploading functionality

--- src/main/kotlin/no/tomlin/api/file/FileController.kt
+++ src/main/kotlin/no/tomlin/api/file/FileController.kt
@@ -5,9 +5,11 @@ import no.tomlin.api.common.Constants.USER
 import no.tomlin.api.file.entity.FileModel
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.file.StandardCopyOption.REPLACE_EXISTING
 import javax.servlet.http.HttpServletResponse
 import kotlin.math.pow
 
@@ -106,6 +108,18 @@ class FileController {
         Files.copy(Paths.get(file.path), response.outputStream)
         response.outputStream.flush()
     }
+
+    @Secured(USER, ADMIN)
+    @PostMapping("/upload")
+    fun upload(@RequestParam path: String, @RequestParam files: Array<MultipartFile>): Int =
+        files.count {
+            val file = getValidFile("$path/${it.originalFilename}")
+
+            checkNotExists(file)
+
+            Files.copy(it.inputStream, file.toPath(), REPLACE_EXISTING)
+            true
+        }
 
     companion object {
         private const val ROOT = "files"
