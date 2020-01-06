09d164ba51f0faa04885dded5a2774194bcbc887
benct <ben@tomlin.no>
2020-01-06 03:19:42 +0100

    Use encrypted passwords with BCrypt

--- src/main/kotlin/no/tomlin/api/config/SecurityConfig.kt
+++ src/main/kotlin/no/tomlin/api/config/SecurityConfig.kt
@@ -1,10 +1,12 @@
 package no.tomlin.api.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -19,4 +21,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             .and()
             .httpBasic()
     }
+
+    @Bean
+    fun encoder() = BCryptPasswordEncoder(4)
 }
