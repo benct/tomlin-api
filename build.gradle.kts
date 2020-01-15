deda3e11df7b33d85e9b40d60e9bc86d6323fbaa
benct <ben@tomlin.no>
2020-01-15 23:58:15 +0100

    Use Java 11 source compat

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "no.tomlin"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 val developmentOnly by configurations.creating
 configurations {
