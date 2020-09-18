610b02a152842839ec57b6abdbeec6536ac5a624
benct <ben@tomlin.no>
2020-09-18 15:30:43 +0200

    Upgrade all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.2.2.RELEASE"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    kotlin("jvm") version "1.3.61"
-    kotlin("plugin.spring") version "1.3.61"
-    id("com.github.ben-manes.versions") version "0.27.0"
+    id("org.springframework.boot") version "2.3.4.RELEASE"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
+    id("com.github.ben-manes.versions") version "0.33.0"
 }
 
 group = "no.tomlin"
 version = "1.0.0"
-java.sourceCompatibility = JavaVersion.VERSION_11
-
-val developmentOnly by configurations.creating
-configurations {
-    runtimeClasspath {
-        extendsFrom(developmentOnly)
-    }
-}
+java.sourceCompatibility = JavaVersion.VERSION_14
 
 repositories {
     mavenCentral()
@@ -29,12 +22,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.2")
-    implementation("com.squareup.okhttp3:okhttp:4.3.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.2")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("mysql:mysql-connector-java:8.0.19")
+    runtimeOnly("mysql:mysql-connector-java:8.0.21")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
