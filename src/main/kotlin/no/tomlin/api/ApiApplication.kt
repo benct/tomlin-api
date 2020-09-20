3758caf804c43232d0ea5b7885dca3d9d1d4bdbb
benct <ben@tomlin.no>
2020-09-20 22:21:47 +0200

    Add scheduler for regular price tracking

--- src/main/kotlin/no/tomlin/api/ApiApplication.kt
+++ src/main/kotlin/no/tomlin/api/ApiApplication.kt
@@ -3,8 +3,10 @@ package no.tomlin.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication
 class ApiApplication
 
