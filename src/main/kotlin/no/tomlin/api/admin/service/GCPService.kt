f1899d071fddf93838dba6d5714aa3ef2e7a61dd
benct <ben@tomlin.no>
2022-12-20 23:06:04 +0100

    Try using GCP sqladmin service for database actions

--- src/main/kotlin/no/tomlin/api/admin/service/GCPService.kt
+++ src/main/kotlin/no/tomlin/api/admin/service/GCPService.kt
@@ -1,5 +1,13 @@
 package no.tomlin.api.admin.service
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.HttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.sqladmin.SQLAdmin
+import com.google.api.services.sqladmin.model.DatabaseInstance
+import com.google.api.services.sqladmin.model.Operation
 import no.tomlin.api.config.ApiProperties
 import no.tomlin.api.http.HttpFetcher
 import no.tomlin.api.http.HttpFetcher.Companion.fetcher
@@ -15,7 +23,36 @@ class GCPService(val fetcher: HttpFetcher = fetcher(GCP_SQL_URL)) {
     @Autowired
     private lateinit var properties: ApiProperties
 
-    fun handleDatabaseStartStop(action: String): ResponseEntity<String> {
+    fun handleDatabaseStartStop(action: String): Operation {
+        val response = when (action) {
+            "start" -> updateGCPDatabase(ActivationPolicy.ALWAYS)
+            "stop" -> updateGCPDatabase(ActivationPolicy.NEVER)
+            "restart" -> updateGCPDatabase(ActivationPolicy.ALWAYS, restart = true)
+            else -> throw IllegalArgumentException("Invalid action '$action' provided.")
+        }
+        return response
+    }
+
+    private fun updateGCPDatabase(policy: ActivationPolicy, restart: Boolean = false): Operation {
+        val sqlAdminService: SQLAdmin = createSqlAdminService()
+        val request = if (restart) {
+            sqlAdminService.instances().restart(PROJECT_ID, properties.dbInstance)
+        } else {
+            val requestBody = DatabaseInstance().apply {
+                settings.activationPolicy = policy.name
+            }
+            sqlAdminService.instances().patch(PROJECT_ID, properties.dbInstance, requestBody)
+        }
+
+        val response: Operation = request.execute()
+
+        // TODO: Change code below to process the `response` object:
+        println(response)
+
+        return response
+    }
+
+    fun _handleDatabaseStartStop(action: String): ResponseEntity<String> {
         val response = when (action) {
             "start" -> runSqlAction(ActivationPolicy.ALWAYS)
             "stop" -> runSqlAction(ActivationPolicy.NEVER)
@@ -39,5 +76,17 @@ class GCPService(val fetcher: HttpFetcher = fetcher(GCP_SQL_URL)) {
         }
 
         fun body(policy: ActivationPolicy) = """{"settings":{"activationPolicy":"${policy.name}"}}"""
+
+        fun createSqlAdminService(): SQLAdmin {
+            val httpTransport: HttpTransport = GoogleNetHttpTransport.newTrustedTransport()
+            val jsonFactory: JsonFactory = GsonFactory.getDefaultInstance()
+            var credential: GoogleCredential = GoogleCredential.getApplicationDefault()
+            if (credential.createScopedRequired()) {
+                credential = credential.createScoped(listOf("https://www.googleapis.com/auth/cloud-platform"))
+            }
+            return SQLAdmin.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("Tomlin-API/1.0")
+                .build()
+        }
     }
 }
