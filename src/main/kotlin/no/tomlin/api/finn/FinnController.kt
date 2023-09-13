c05486c63a899bf97d17915c4282d218f314fe8f
benct <ben@tomlin.no>
2023-09-13 19:20:40 +0200

    Move caching to the business layer

--- src/main/kotlin/no/tomlin/api/finn/FinnController.kt
+++ src/main/kotlin/no/tomlin/api/finn/FinnController.kt
@@ -5,6 +5,8 @@ import no.tomlin.api.common.Constants.PRIVATE
 import no.tomlin.api.common.JsonUtils.parseJson
 import no.tomlin.api.http.HttpFetcher
 import no.tomlin.api.http.HttpFetcher.Companion.fetcher
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.*
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*
 class FinnController(private val finnDao: FinnDao, private val fetcher: HttpFetcher = fetcher(FINN_URL)) {
 
     @Secured(ADMIN, PRIVATE)
+    @Cacheable("finn")
     @GetMapping
     fun list(): Map<String, List<Map<String, Any>>> = finnDao.get()
         .fold(mutableMapOf<String, MutableList<Map<String, Any>>>()) { accumulator, entry ->
@@ -29,14 +32,17 @@ class FinnController(private val finnDao: FinnDao, private val fetcher: HttpFetc
     fun get(@PathVariable id: Long): List<Map<String, Any?>> = finnDao.get(id)
 
     @Secured(ADMIN, PRIVATE)
+    @CacheEvict("finn", allEntries = true)
     @PostMapping
     fun track(): Boolean = trackAllPrices()
 
     @Secured(ADMIN, PRIVATE)
+    @CacheEvict("finn", allEntries = true)
     @PostMapping("/{id}")
     fun track(@PathVariable id: Long): Boolean = finnDao.save(id, fetchPrice(id))
 
     @Secured(ADMIN, PRIVATE)
+    @CacheEvict("finn", allEntries = true)
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long): Boolean = finnDao.delete(id)
 
--- src/main/kotlin/no/tomlin/api/finn/FinnDao.kt
+++ src/main/kotlin/no/tomlin/api/finn/FinnDao.kt
@@ -2,8 +2,6 @@ package no.tomlin.api.finn
 
 import no.tomlin.api.common.Constants.TABLE_FINN
 import no.tomlin.api.common.Extensions.checkRowsAffected
-import org.springframework.cache.annotation.CacheEvict
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
@@ -11,7 +9,6 @@ import org.springframework.stereotype.Repository
 @Repository
 class FinnDao(private val jdbcTemplate: NamedParameterJdbcTemplate) {
 
-    @Cacheable("finn")
     fun get(): List<Map<String, Any>> = jdbcTemplate.queryForList(
         "SELECT * FROM $TABLE_FINN ORDER BY `id` DESC, `timestamp` DESC",
         EmptySqlParameterSource.INSTANCE
@@ -23,12 +20,10 @@ class FinnDao(private val jdbcTemplate: NamedParameterJdbcTemplate) {
     fun getUniqueIds(): List<Long> = jdbcTemplate
         .queryForList("SELECT DISTINCT(`id`) FROM $TABLE_FINN", EmptySqlParameterSource.INSTANCE, Long::class.java)
 
-    @CacheEvict("finn", allEntries = true)
     fun save(id: Long, price: String): Boolean = jdbcTemplate
         .update("INSERT INTO $TABLE_FINN (id, price) VALUES (:id, :price)", mapOf("id" to id, "price" to price))
         .checkRowsAffected()
 
-    @CacheEvict("finn", allEntries = true)
     fun delete(id: Long): Boolean = jdbcTemplate
         .update("DELETE FROM $TABLE_FINN WHERE id = :id", mapOf("id" to id)) > 0
 }
