7843ef3c5f87c9f42018b51869c3ed04f4d6f9be
benct <ben@tomlin.no>
2023-09-17 00:26:25 +0200

    Remove deprecated db extensions and table constants

--- src/main/kotlin/no/tomlin/api/common/Constants.kt
+++ src/main/kotlin/no/tomlin/api/common/Constants.kt
@@ -13,26 +13,4 @@ object Constants {
 
     // Pagination
     const val PAGE_SIZE = 50
-
-    // Database tables
-    private const val TABLE_PREFIX = "tomlin_"
-    const val TABLE_SETTINGS = "${TABLE_PREFIX}settings"
-    const val TABLE_USER = "${TABLE_PREFIX}user"
-    const val TABLE_ROLE = "${TABLE_PREFIX}role"
-    const val TABLE_LOG = "${TABLE_PREFIX}log"
-    const val TABLE_TRACK = "${TABLE_PREFIX}track"
-    const val TABLE_NOTE = "${TABLE_PREFIX}note"
-    const val TABLE_FLIGHT = "${TABLE_PREFIX}flight"
-    const val TABLE_BEEN = "${TABLE_PREFIX}been"
-    const val TABLE_FINN = "${TABLE_PREFIX}finn"
-    const val TABLE_QRATOR = "${TABLE_PREFIX}qrator"
-    const val TABLE_RATING = "${TABLE_PREFIX}rating"
-    const val TABLE_RATING_ITEM = "${TABLE_PREFIX}rating_item"
-    const val TABLE_RATING_SCORE = "${TABLE_PREFIX}rating_score"
-    const val TABLE_MOVIE = "${TABLE_PREFIX}movie"
-    const val TABLE_TV = "${TABLE_PREFIX}tv"
-    const val TABLE_SEASON = "${TABLE_PREFIX}season"
-    const val TABLE_EPISODE = "${TABLE_PREFIX}episode"
-    const val TABLE_AIRLINE = "${TABLE_PREFIX}iata_airline"
-    const val TABLE_LOCATION = "${TABLE_PREFIX}iata_location"
 }
--- src/main/kotlin/no/tomlin/api/common/Extensions.kt
+++ src/main/kotlin/no/tomlin/api/common/Extensions.kt
@@ -10,12 +10,6 @@ object Extensions {
 
     fun List<String>.nullIfBlank(): List<String?> = this.map { if (it.isBlank()) null else it.trim() }
 
-    fun Int.checkRowsAffected(): Boolean =
-        if (this !in 1..2) throw AffectedIncorrectNumberOfRowsException(this) else true
-
     fun Long.formatDuration(): String =
         Duration.ofMillis(this).let { "${it.toHoursPart()}h ${it.toMinutesPart()}m ${it.toSecondsPart()}s" }
-
-    class AffectedIncorrectNumberOfRowsException(actual: Int, min: Int = 1, max: Int = 2) :
-        RuntimeException("SQL update affected $actual rows, not in range [$min, $max]")
 }
