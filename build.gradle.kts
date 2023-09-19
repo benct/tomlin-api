6f953041c7fc11c92927ad9af8ce04449f2783aa
benct <ben@tomlin.no>
2023-09-20 01:57:12 +0200

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.7.14"
-    id("io.spring.dependency-management") version "1.1.2"
+    id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.spring") version "1.9.0"
-    id("com.github.ben-manes.versions") version "0.47.0"
+    id("com.github.ben-manes.versions") version "0.48.0"
 }
 
 group = "no.tomlin"
@@ -26,8 +26,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.google.cloud.sql:mysql-socket-factory-connector-j-8:1.13.1")
-    implementation("com.google.cloud:google-cloud-storage:2.26.0")
+    implementation("com.google.cloud.sql:mysql-socket-factory-connector-j-8:1.14.0")
+    implementation("com.google.cloud:google-cloud-storage:2.27.0")
     implementation("com.google.zxing:core:3.5.2")
     implementation("com.google.zxing:javase:3.5.2")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
